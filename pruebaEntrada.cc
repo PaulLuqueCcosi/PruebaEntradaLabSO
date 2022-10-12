cd1693091dede0f822eafc365dcfa86034d771bf
PaulLuqueCcosi <pluquec@unsa.edu.pe>
2022-10-12 15:32:10 -0500

    funciona las llamadas a los metdos de las clases superiores

--- pruebaEntrada.cc
+++ pruebaEntrada.cc
@@ -3,7 +3,7 @@ using namespace std;
 
 class A{
 	public:
-		void foo(){
+		virtual void foo(){
 			cout << "A" << endl;
 		}
 
@@ -11,14 +11,14 @@ class A{
 
 class  B: public A{
 	public:
-		void foo(){
+		virtual void foo(){
 			cout << "B" << endl;
 		}
 };
 
 class C: public A{
 	public:
-		void goo(){
+		virtual void goo(){
 			cout << "C" << endl;
 		}
 };
@@ -44,8 +44,45 @@ int main(){
 	C *c = new C();
 	D *d = new D();
 	E *e = new E();
-	
-	A a1;
-	a1  = *e;
-	a1.foo();
+
+	cout << "----------" << endl;
+	cout << "a->foo()" << endl;
+	a->foo();
+	cout << "b->foo()" << endl;
+	b->foo();
+	cout << "c->foo()" << endl;
+	c->foo();
+	cout << "c->goo()" << endl;
+	c->goo();
+	cout << "d->foo()" << endl;
+	d->foo();
+	cout << "d->goo()" << endl;
+	d->goo();
+	cout << "e->foo()" << endl;
+	e->foo();
+	cout << "e->goo()" << endl;
+	e->goo();
+
+	cout << "----------" << endl;
+
+	/*
+		 A a1;
+		 a1  = *e;
+		 cout << "e->foo()" << endl;
+		 e->foo();
+		 cout << "e.goo()" << endl;
+		 e->goo();
+		 cout << "e1.foo()" << endl;
+		 E e1;
+		 e1 = *a; // error	en la conversion
+		 e1.foo();
+		 cout << "a1.foo()" << endl;
+		 a1.foo();
+		 cout << "a1.goo()" << endl;
+	//a1.goo(); // error, la clase A no tiene el metodo go
+	cout << "e->goo()" << endl;
+	e->goo();
+	cout << "c->foo()" << endl;
+	c->foo();
+	*/
 }
